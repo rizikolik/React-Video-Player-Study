f950443724e0539708083a5e891fdef32555dfd4
yunustr <ynsture@gmail.com>
2019-06-30 00:36:09 +0300

    added new files and updated sources

--- src/index.js
+++ src/index.js
@@ -1,12 +1,41 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { Video } from './Video';
+import { Menu } from './Menu';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const VIDEOS = {
+  fast: 'https://s3.amazonaws.com/codecademy-content/courses/React/react_video-fast.mp4',
+  slow: 'https://s3.amazonaws.com/codecademy-content/courses/React/react_video-slow.mp4',
+  cute: 'https://s3.amazonaws.com/codecademy-content/courses/React/react_video-cute.mp4',
+  eek: 'https://s3.amazonaws.com/codecademy-content/courses/React/react_video-eek.mp4'
+};
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { src: VIDEOS.fast };
+    this.chooseVideo=this.chooseVideo.bind(this);
+  }
+  chooseVideo(newVideo){
+   
+    this.setState({
+      src:VIDEOS[newVideo]
+    })
+  }
+  
+  render() {
+    return (
+      <div>
+        <h1>Video Player</h1>
+        <Menu  chooseVideo={this.chooseVideo}/>
+        <Video src={this.state.src}/>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(
+  <App />, 
+  document.getElementById('app')
+);
